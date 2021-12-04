6f7e7c97ccd60aac45589e8b9f2a22562db91ba0
”Klyuev <11sergey93@mail.ru>
2021-12-04 17:08:22 +0300

    - added FileDto - used bucket name

--- src/main/java/ru/klyuev/s3miniodemo/services/MinioAdapter.java
+++ src/main/java/ru/klyuev/s3miniodemo/services/MinioAdapter.java
@@ -1,21 +1,42 @@
 package ru.klyuev.s3miniodemo.services;
 
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
-import io.minio.messages.Bucket;
+import io.minio.Result;
+import io.minio.messages.Item;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.klyuev.s3miniodemo.utils.FileDto;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class MinioAdapter {
+public class MinioAdapter implements Serializable {
 
     @Autowired
-    MinioClient minioClient;
+    private MinioClient minioClient;
 
-    public List<Bucket> getAllBucket() {
+    @Value("${minio.bucket}")
+    private String bucket;
+
+    public List<FileDto> getAllBucket() {
+        List<FileDto> objects = new ArrayList<>();
         try {
-            return minioClient.listBuckets();
+            Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder()
+                    .bucket(bucket)
+                    .recursive(true)
+                    .build());
+            for (Result<Item> item : results) {
+                FileDto fileDto = new FileDto();
+//                fileDto.setFilename(item.get().objectName());
+//                fileDto.setSize(item.get().size());
+                objects.add(fileDto);
+            }
+            return objects;
         } catch (Exception ex) {
             throw new RuntimeException(ex.getMessage());
         }
---
+++ src/main/java/ru/klyuev/s3miniodemo/utils/FileDto.java
@@ -0,0 +1,23 @@
+package ru.klyuev.s3miniodemo.utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FileDto implements Serializable {
+
+        private String filename;
+        private Long size;
+}
